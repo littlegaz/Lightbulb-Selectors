fff03db230f001673e9761185f8aa23a97e263ac
littlegaz <gareth.horne@barclays.com>
2024-05-02 10:20:14 +0100

    part 4 and 5 done, on part 6

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ let lightbulb2 = document.querySelector("#lightbulb2")
 let lightbulb3 = document.querySelector("#lightbulb3")
 
 let subtitle = document.querySelector(".subtitle")
+let count = 0
 
+lightbulb1.addEventListener("click", function (){
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+})
+
+lightbulb2.addEventListener("click", function (){
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+})
+
+lightbulb3.addEventListener("click", function (){
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+})
