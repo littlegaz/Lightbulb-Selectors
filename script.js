c910a13076b70fa7dbbddb61ef834944c3e217cc
littlegaz <gareth.horne@barclays.com>
2024-05-02 10:12:23 +0100

    step 1,2,3 done

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// Write your code here
+let lightbulb1 = document.querySelector("#lightbulb1")
+let lightbulb2 = document.querySelector("#lightbulb2")
+let lightbulb3 = document.querySelector("#lightbulb3")
+
+let subtitle = document.querySelector(".subtitle")
+
